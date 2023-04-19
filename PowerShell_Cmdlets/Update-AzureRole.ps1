6a337e8943973778d845c607378c43d8e1f5644c
John Lewis <johnelewis92@gmail.com>
2023-04-19 16:43:32 -0500

    Removed redundant code that checks for type of Object to simply use the Object ID

--- PowerShell_Cmdlets/Update-AzureRole.ps1
+++ PowerShell_Cmdlets/Update-AzureRole.ps1
@@ -1,4 +1,5 @@
 # This Powershell CMDLET is intended to be used to quickly update role assignments on objects in Azure. 
+# Development: testing if the redundant code here can be reduced. 
 #Sample call:
 #
 # PS > Update-AzureRole -Scope 'Resource' -ObjectId '12345678-1234-5678-1234-567890abcdef' -RoleName 'Contributor' -RoleDefinitionId 'b24988ac-6180-41a0-ab88-20f7382dd24c'
@@ -23,57 +24,13 @@ function Update-AzureRole {
     #Authenticate to Azure AD
     Connect-AzureAD
 
-    # Check if the specificed Object ID is a group, user or serviceprincipal / managed identity
-    $isGroup = $false
-    $isUser = $false
-    $isServicePrincipal = $false
-
-    try {
-        $group = Get-AzureADGroup -ObjectId $ObjectId
-        $isGroup = $true
-    } catch {
-        try{
-            $user = Get-AzureADUser -ObjectId -ErrorAction Stop
-            $isUser = $true
-        } catch {
-            $ServicePrincipal = Get-AzureADServicePrincipal -ObjectId $ObjectId -ErrorAction Stop
-            $isServicePrincipal = $true
-        }
-    }
-
-    # Update the role assignment based on the object type
-    if($isGroup){
+    # Update the role assignment using the ObjectId
         # Update role assignment for group
-        $roleAssignment = Get-AzureADMSRoleAssignment -ObjectId $group.ObjectId -RoleDefinitionId $RoleDefinitionId -ErrorAction SilentlyContinue
+        $roleAssignment = Get-AzureADMSRoleAssignment -ObjectId $ObjectId -RoleDefinitionId $RoleDefinitionId -ErrorAction SilentlyContinue
         if ($roleAssignment) {
             $roleAssignment | ForEach-Object {
                 Update-AzureADMSRoleAssignment -Id $_.Id -RoleDefinitionId $RoleDefinitionId
             }
         } else {
-            New-AzureADMSRoleAssignment -ObjectId $group.ObjectId -RoleDefinitionId $RoleDefinitionId
+            New-AzureADMSRoleAssignment -ObjectId $ObjectId -RoleDefinitionId $RoleDefinitionId
         }
-    } elseif ($isUser) {
-        # Update role assignment for user
-        $roleAssignment = Get-AzureADMSRoleAssignment -ObjectId $group.ObjectId -RoleDefinitionId $RoleDefinitionId -ErrorAction SilentlyContinue
-        if ($roleAssignment) {
-            $roleAssignment | ForEach-Object {
-                Update-AzureADMSRoleAssignment -Id $_.Id -RoleDefinitionId $RoleDefinitionId
-            }
-        } else {
-            New-AzureADMSRoleAssignment -ObjectId $user.ObjectId -RoleDefinitionId $RoleDefinitionId
-        }
-    } else {
-        # Upate role assignment for service principal / managed identity
-        $roleAssignment = Get-AzureADMSRoleAssignment -ObjectId $group.ObjectId -RoleDefinitionId $RoleDefinitionId -ErrorAction SilentlyContinue
-        if ($roleAssignment) {
-            $roleAssignment | ForEach-Object {
-                Update-AzureADMSRoleAssignment -Id $_.Id -RoleDefinitionId $RoleDefinitionId
-            }
-        } else {
-            New-AzureADMSRoleAssignment -ObjectId $ServicePrincipal.ObjectId -RoleDefinitionId $RoleDefinitionId
-        }
-    }
-
-
-
-}
