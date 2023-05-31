e018c7de56931878a0c6932bd006c8c3515f1c36
John Lewis <johnelewis92@gmail.com>
2023-05-31 18:29:11 -0500

    Final version of container directory

--- Create-ContainerFolder.ps1
+++ Create-ContainerFolder.ps1
@@ -1,22 +1,28 @@
-#Create and configure Blob Containers
+#Create and configure Blob Container directories. Please create the container first if it doesn't exist
 
 #Set Variables - UPDATE FOR EACH ENVIRONMENT
-$SubscriptionName = "BCBSLA Dev Data and Analytics"
-$ResourceGroupName = "devuscedbxpilotrg"
-$ADLSName = "devuscedbxpilotadls01"
-$ContainerName = "devuscedbxpilotadls01"
-$folderName = "folder1" #can add 
+$SubscriptionName = "Subscription Name"
+$ResourceGroupName = "Resource Group Name"
+$ADLSName = "storage account name"
+$ContainerName = "container name"
+$folderName = "directory name" #can add \ to create subfolders
 $AccessGroups = @(
-    @{AAD_ID ="group"; Permissions = "rwx"},
-    @{AAD_ID =""; Permissions = "rw-"},
-    @{AAD_ID =""; Permissions = "r--"}
+    @{AAD_ID ="AzDev.DnA Storage Inbound ncct Reader"; Permissions = "r-x"},
+    @{AAD_ID ="AzDev.DnA Storage Inbound ncct Writer"; Permissions = "rwx"}
 );
 
 #replace AccessGroups 'AAD_ID' value with Azure AD Group Object ID
 write-host "Replacing AccessGroups 'AAD_ID' value with Azure AD Group Object ID" -ForegroundColor Green
 foreach ($group in $AccessGroups) {
-    $group.AAD_ID = (Get-AzADGroup -SearchString $group.AAD_ID).Id
+    if(Get-AzADGroup -SearchString $group.AAD_ID){
+        $group.AAD_ID = (Get-AzADGroup -SearchString $group.AAD_ID).Id
+    } elseif (Get-AzADApplication -DisplayName $group.AAD_ID){
+        $group.AAD_ID = (Get-AzADApplication -DisplayName $group.AAD_ID).Id
+    } else {
+        write-host "AAD_ID not found for $group.AAD_ID" -ForegroundColor Red
+    }
 }
+$AccessGroups
 
 
 #Set subscription context
@@ -25,17 +31,19 @@ Set-AzContext -SubscriptionName $SubscriptionName
 #Get Storage Account Context
 $context = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $ADLSName).Context
 
-#Create blob container folder if it doesn't exist
-write-host "Creating $ContainerName\$folderName Blob Container Folder!" -ForegroundColor Green
-if (Get-AzStorageContainer -Name "$ContainerName\$folderName" -Context $context -ErrorAction SilentlyContinue) {
-    write-host "$ContainerName\$folderName Blob Container Folder already exists!" -ForegroundColor Yellow
-} else {
-    New-AzStorageContainer -Name "$ContainerName\$folderName" -Context $context -Permission Off -ErrorAction Stop
+#if azdatalakegen2item doesn't exist, create it
+if (!(Get-AzDataLakeGen2Item -Context $context -FileSystem $ContainerName -Path $folderName -ErrorAction SilentlyContinue)) {
+    write-host "Creating $folderName in $ContainerName" -ForegroundColor Green
+    New-AzDataLakeGen2Item -Context $context -FileSystem $ContainerName -Path $folderName -Directory -ErrorAction Stop
 }
 
-
-#Iterate over the array and set the ACL for each folder
+#Iterate over the array and build an ACL Object
+write-host "Iterate over the array and build an ACL Object" -ForegroundColor Green
 foreach ($group in $AccessGroups) {
-    write-host "Setting ACL for $($group.AAD_ID) to $($group.Permissions)" -ForegroundColor Green
-    Set-AzStorageContainerAcl -Name "$ContainerName\$folderName" -Context $context -Group $group.AAD_ID -Permission $group.Permissions -ErrorAction Stop
+    #Please note that default ACLs and the actual entity ACLs are two separate things and must be set seprately as shown below
+    $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType group -EntityId $group.AAD_ID -Permission $group.Permissions -DefaultScope  -ErrorAction Stop
+    Update-AzDataLakeGen2Item -Context $context -FileSystem $ContainerName -Path $folderName -Acl $acl -ErrorAction Stop
+    $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType group -EntityId $group.AAD_ID -Permission $group.Permissions -ErrorAction Stop
+    Update-AzDataLakeGen2Item -Context $context -FileSystem $ContainerName -Path $folderName -Acl $acl -ErrorAction Stop
 }
+#Update-AzDataLakeGen2Item -Context $context -FileSystem $ContainerName -Path $folderName -Acl $acl -ErrorAction Stop
