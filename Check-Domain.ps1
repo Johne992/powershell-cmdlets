65a2ac5eedef56231785efb135214823dd183d22
John Lewis <johnelewis92@gmail.com>
2023-07-10 11:19:26 -0500

    check-domain

--- Check-Domain.ps1
+++ Check-Domain.ps1
@@ -4,7 +4,7 @@ foreach ($server in $servers) {
     $server
     $domain = (Get-WmiObject Win32_ComputerSystem -ComputerName $server).Domain
     $domain
-    if ($domain -eq "lahsic.com") {
+    if ($domain -eq "domain") {
         write-host "Domain is domain1" -ForegroundColor Green
         #add to output file
         Add-Content -Path "C:\directory\Data\Data\domain.txt" -Value $server
